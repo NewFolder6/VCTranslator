20ec7c7b396ec5983cef1e84b9940162008cd8be
New Folder (6) <150495167+NewFolder6@users.noreply.github.com>
2025-02-17 00:35:06 -0800

    Initial commit

---
+++ VCTranslator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.IO;
+using Vosk;
+
+public class Program
+{
+    private Model model;
+    private VoskRecognizer recognizer;
+    private int SAMPLE_RATE = 16000;
+    
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
